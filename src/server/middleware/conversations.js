f949cf5119f9e7ca1b0953fd7ef85315eba1114e
David Cohen <davidmartincohen@gmail.com>
2023-09-21 15:30:39 +0200

    ensure middleware only applies to conversations/:senderId route

--- src/server/middleware/conversations.js
+++ src/server/middleware/conversations.js
@@ -4,8 +4,12 @@ const db = require(`${path.dirname(__filename)}/../db.json`);
 // Need this middleware to catch some requests
 // and return both conversations where userId is sender or recipient
 module.exports = (req, res, next) => {
-  if (/conversations/.test(req.url) && req.method === "GET") {
-    const userId = req.query?.senderId;
+  if (
+    /conversations/.test(req.url) &&
+    req.method === "GET" &&
+    req.query?.senderId
+  ) {
+    const userId = req.query.senderId;
 
     const result = db?.conversations?.filter(
       (conv) => conv.senderId == userId || conv.recipientId == userId,
